faf00a098f7cbf5e7cf793a02108fdef2bd8e07e
ZoonCH <adityagtps951@gmail.com>
2022-08-03 22:30:24 +0700

    v11

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,43 +1,47 @@
-/*let limit = 202
-let fetch = require('node-fetch')
-const { servers, ytv } = require('../lib/y2mate')
+/*let limit = 30
+const fetch = require('node-fetch')
+// const { servers, ytv } = require('../lib/y2mate')
+const { youtubedl, youtubedlv2 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
   if (!args || !args[0]) throw 'Uhm... urlnya mana?'
   let chat = global.db.data.chats[m.chat]
-  let server = (args[1] || servers[0]).toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 202 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-*${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
+  let isY = /y(es)/gi.test(args[1])
+  const { thumbnail, video: _video, title } = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0]))
+  let video, link = '', lastError
+  for (let i in _video) {
+    try {
+      video = _video[i]
+      link = await video.download()
+      if (link) break
+    } catch (e) {
+      lastError = e
+      continue
+    }
+  }
+  if (!link) throw lastError
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < video.fileSize
+  if (!isY) conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
+📌*Title:* ${title}
+🗎 *Filesize:* ${video.fileSizeH}
+*${isLimit ? 'Pakai ' : ''}Link:* ${link}
 `.trim(), m)
   let _thumb = {}
-  try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
+  try { _thumb = { thumbnail: await (await fetch(thumbnail)).buffer() } }
   catch (e) { }
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
+  if (!isLimit) conn.sendFile(m.chat, link, title + '.mp4', `
+📌 *Title:* ${title}
+🗎 *Filesize:* ${video.fileSizeH}
 `.trim(), m, false, {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+    ..._thumb,
+    asDocument: chat.useDocument
+  })
 }
-handler.help = ['mp4','v',''].map(v => 'yt' + v + ` <url>`)
+handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> <without message>`)
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
 handler.exp = 0
-handler.limit = 15
+
 
 module.exports = handler*/
 
@@ -50,7 +54,7 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 100 < filesize
+  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
   conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
 *Title:* ${title}
 *Filesize:* ${filesizeF}
@@ -81,31 +85,12 @@ handler.botAdmin = false
 
 handler.fail = null
 handler.exp = 0
-handler.limit = 10
+handler.limit = true
 
 module.exports = handler
 
 async function shortlink(url) {
 isurl = /https?:\/\//.test(url)
 return isurl ? (await require('axios').get('https://tinyurl.com/api-create.php?url='+encodeURIComponent(url))).data : ''
-}*/
-     let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url: u }))).buffer()
-     let res = await mediafiredl(args[0])
-     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-     let isLimit = (isPrems || isOwner ? limit : limit) * 1024 < filesize
-     let error = `Gagal mendownload..`
-     let capt = `📭 *Mediafire Downloader*
-
-📝 *Name:* ${filename}
-🎚 *Size:* ${filesizeH}
-🗃 *Extension:* ${ext}
-📤 *Uploaded:* ${aploud}
-${isLimit ? `❌ *Ukuran file di atas ${limit} MB, download sendiri*\n` : ''}🚀 *Link:* ${await(await axios.get(`https://tinyurl.com/api-create.php?url=${url}`)).data}` 
-     
-     if(ss) await conn.sendFile(m.chat, ss, 'screenshot.png', capt, sentMsg, 0, {jpegThumbnail: ss})
-     try {
-     if(!isLimit) await conn.sendMedia(m.chat, url, 0, {fileName: `${filename}`, mentions: [m.sender]})
-     } catch {
-      throw error
-  }
 }
+
