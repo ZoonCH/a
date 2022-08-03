edf10b553f2f3fe7586dc9e14c795b610b4230db
ZoonCH <adityagtps951@gmail.com>
2022-08-03 14:32:38 +0700

    pluto

---
+++ plugins/inf-sc.js
@@ -0,0 +1,10 @@
+let handler = async m => m.reply(`
+            Nyari sc nya yaa
+`.trim()) //
+
+handler.customPrefix = /^(.sc|.script|sc|script)$/i
+handler.command = new RegExp
+
+handler.limit = false
+handler.mods = false 
+module.exports = handler
